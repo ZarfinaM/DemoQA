bae52c756c8cb0edef179ae2f89cb7f610e0db96
Zarfina Mulalic <zarfina.mulalic@gmail.com>
2023-05-16 13:57:03 +0200

    Transferred common steps to a folder created for them

--- CommonStepsDemoQA/CommonSteps.cs
+++ CommonStepsDemoQA/CommonSteps.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DemoQa;
+using OpenQA.Selenium;
 
-namespace DemoQA.CommonSteps
+namespace DemoQA.CommonStepsDemoQa
+
 {
-    internal class CommonSteps
+    public static class CommonSteps
     {
+        public static void GoogleSearchDemoQa()
+        {
+            var googleSearch = Driver.Instance.FindElement(By.Name("q"));
+            googleSearch.Click();
+            googleSearch.SendKeys("demoqa");
+            googleSearch.SendKeys(Keys.Enter);
+        }
+
+        public static void DemoQa()
+        {
+            var demoQA = Driver.Instance.FindElement(By.CssSelector("#rso > div:nth-child(1) > div > div > div > div > div > div > div.yuRUbf > a > h3"));
+            demoQA.Click();
+        }
+
+        public static void Email()
+        {
+            var email = Driver.Instance.FindElement(By.CssSelector("#userEmail"));
+            email.Click();
+            email.SendKeys("zarfina.mulalic@gmail.com");
+        }
+
+        public static void CurrentAddress()
+        {
+            var currentAddress = Driver.Instance.FindElement(By.XPath("//*[@id=\"currentAddress\"]"));
+            currentAddress.Click();
+            currentAddress.SendKeys("Sarajevo");
+        }
+
+        public static void SubmitButton()
+        {
+            var submitButton = Driver.Instance.FindElement(By.CssSelector("#submit"));
+            submitButton.Click();
+        }
+
+        public static void UploadChooseImage()
+        {
+            var uploadInput = Driver.Instance.FindElement(By.ClassName("form-control-file"));
+            uploadInput.SendKeys(@"C:\Users\Zarfina\OneDrive\Radna površina\Cvijet - Test.jpg");
+
+        }
     }
 }
